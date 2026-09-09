dabd5c60eba16b263693e6477b93eeaec166fc69
abhishekchauhan1365 <143802835+abhishekchauhan1365@users.noreply.github.com>
2026-09-09 12:18:15 +0530

    Time: 0 ms (100%), Space: 9 MB (84.8%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
-    long long countCommas(long long n){
+    long long countCommas(long long n) {
         long long count=0;
-        long long b=n;
-        int a=0;
-        while(n>=0){
-        int y=n%10;
-        n/=10;
-        count++;
+        long long place=1000;
+        while(n>=place){
+            count+=(n-place+1);
+            place*=1000;
         }
-
-        if(count<3){
-            a=0;
-        }
-        if(count==4){
-            b=b-1000;
-            a=b;
-        }
-        if(count==5){
-            b-b-10000;
-            a=b;
-        }
-
-        return a;
+        return count;
     }
 };
